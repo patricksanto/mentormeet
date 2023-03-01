184cf8ae163a0ae83b00567a284128f64b5ace7f
Daniel Lameira <dlameira@gmail.com>
2023-03-01 18:11:27 -0300

    validates mentorship partial

--- app/models/mentorship.rb
+++ app/models/mentorship.rb
@@ -1,3 +1,12 @@
 class Mentorship < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  has_many :bookings
+  validates :title, length: { minimum: 30 }
+  validates :content, presence: true
+  validates :place, presence: true
+  validates :is_remote
+  validates :is_hybrid
+
+
 end
