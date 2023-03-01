43d01dbeea064c10e71e15dbd5eb0ce1c41e80d3
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-03-01 18:27:09 -0300

    validating booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :mentorship
+  has_one :review
+  validates :date, presence: true
+  validates :is_accepted, inclusion: { in: [ true, false ] }
+
+
+
 end
+
+
+
