9e9bb51bd0cef38fb99e01eb0d07e8a98dabd41c
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-01 18:28:56 -0300

    creating the validations for  user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
+  validates :birthday, presence: true
+
 end
