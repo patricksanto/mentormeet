499ea492f4ad0e9a25ff6d4945656455918b7720
fernando santiago <fer.santiago16@gmail.com>
2023-03-01 18:10:59 -0300

    Validates Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :mentorship
   belongs_to :booking
+  validates :title, presence: true
+  validates :rating, presence: true
+  validates :content, length: { minimum: 30 }
 end
