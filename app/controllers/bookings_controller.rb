798a23650a0b2a67c869f53752abda350e218425
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-03-02 11:50:08 -0300

    incomplete booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,58 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  #INCOMPLETE CONTROLLER
+
+  def index
+    @bookings = Booking.all
+
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    authorize @booking
+    if @booking.save
+      redirect_to  mentorships_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    authorize @restaurant
+  end
+
+  def update
+    if @booking.update(booking_params)
+
+      redirect_to mentorships_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @booking
+    @booking.destroy
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:id_mentorship, :id_user, :date, :is_accepted)
+  end
 end
+
+
+
