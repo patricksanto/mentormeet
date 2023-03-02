68bdc8f6f0fc4755f421197cceafe587bf44ed73
fernando santiago <fer.santiago16@gmail.com>
2023-03-01 22:21:27 -0300

    Added validates to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
   belongs_to :mentorship
   belongs_to :booking
   validates :title, presence: true
+  validates :title, length: { minumum: 5 }
   validates :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :content, length: { minimum: 30 }
 end
