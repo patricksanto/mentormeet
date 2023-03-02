09620150bfb8cb0af7ff65938eebedbdb3b20d52
Daniel Lameira <dlameira@gmail.com>
2023-03-02 11:42:17 -0300

    authorize policies mentorship done

--- app/policies/mentorship_policy.rb
+++ app/policies/mentorship_policy.rb
@@ -1,8 +1,34 @@
 class MentorshipPolicy < ApplicationPolicy
   class Scope < Scope
+    def resolve
+      scope.all
+    end
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
     #   scope.all
     # end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.admin
+  end
+
+  def update?
+    owner?
+  end
+
+  def destroy?
+    owner?
+  end
+
+  def owner?
+    # restaurant user should be the same as logged in user
+    # @restaurant.user == current_user
+    # raise
+    record.user == user
   end
 end
