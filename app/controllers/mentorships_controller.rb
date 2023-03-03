54147fdbf0e20e6ec5083b1197763e325c560273
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-03 11:41:57 -0300

    some style in the mentorship page done , fixed some issues with cloudnary in the mentorship model

--- app/controllers/mentorships_controller.rb
+++ app/controllers/mentorships_controller.rb
@@ -49,6 +49,6 @@ class MentorshipsController < ApplicationController
   end
 
   def mentorship_params
-    params.require(:mentorship).permit(:title, :content, :place)
+    params.require(:mentorship).permit(:title, :content, :place, :photo)
   end
 end
