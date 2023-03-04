bde9a3f4107675d2f6758e82014e5fde3fe6f9bd
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-04 18:22:56 -0300

    dashboard created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     authorize @user
   end
+
+  def dashboard
+    @user = current_user
+    authorize @user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :reviews, only: [:destroy]
   resources :users, only: [:show]
 
+  get "dashboard", to: "users#dashboard"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
