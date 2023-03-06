b8e044f3013cc8bf74cd4a1981c79869748372e8
fernando santiago <fer.santiago16@gmail.com>
2023-03-06 15:34:47 -0300

    botei message no schedule e link to dashboard na foto do mentorship

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :mentorship
   has_one :review
 
-  enum status: [ :pending, :accepted, :denied ]
+  enum status: %i[pending accepted denied]
   validates :date, presence: true
+  validates :message, presence: true
+  validates :message, length: { minimum: 10 }
+  validates :message, length: { maximum: 50 }
 end
