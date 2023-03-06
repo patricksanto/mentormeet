73decfcd4cc84533c2ed0a6f42913e366816432e
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-06 15:49:55 -0300

    meio do caminho

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   get "dashboard", to: "users#dashboard"
 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
