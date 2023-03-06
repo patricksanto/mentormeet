b1334f0ea565c01030e9e25a5f557a01fa5b9590
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-06 12:43:34 -0300

    change the statys in the bookings table in the db

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :mentorship
   has_one :review
+
+  enum status: [ :pending, :accepted, :denied ]
   validates :date, presence: true
   validates :is_accepted, inclusion: { in: [ true, false ] }
 end
